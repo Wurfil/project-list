c948e94737efcb401b71239336360d1553b4e1d8
Sokolov <man.mr.hamster.man@gmail.com>
2023-02-22 22:39:17 +0300

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import './index.less'
+import React from "react";
+import {render} from "react-dom";
+
+const App = () => {
+    return (
+        <div>
+            Doing...
+        </div>
+    );
+};
+
+export default App;
